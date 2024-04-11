21986c94066e5e45d1bf60f734ea087be066a83d
airwin17 <faitourdja@gmail.com>
2024-04-11 21:49:38 +0200

    fixed code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,43 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	static String res="";
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+		String filepath=args[0];
+		HashMap<String,Integer> result = new HashMap<String,Integer>();
+		LinkedList<String> tabout=new LinkedList<String>();
+		if (filepath != null) {
+			try {
+				BufferedReader reader=new BufferedReader(new FileReader(filepath));
+				while (reader.ready()) {
+					String str=reader.readLine();
+					if(!result.containsKey(str)){
+						result.put(str, 1);
+						tabout.add(str);
+					}else
+						result.put(str,result.get(str)+1);
+				}
+				reader.close();
+				Collections.sort(tabout);
+				tabout.stream().forEach(i->{res+= result.get(i) +" "+i+" found \n";});
+				FileWriter writer = new FileWriter ("result.out");
+				writer.write(res);
+				writer.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("inputfile not found");
+			} catch ( IOException e){
+				System.out.println("reading error chack if file is not corrupted and not used by others applications");
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
 		}
-		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 }
