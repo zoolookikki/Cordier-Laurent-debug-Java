3e8338a1ac222a2ecce96875b2d0ff7c2259ffb1
nicolas cretton <nicolas.cretton@orange.fr>
2024-05-05 18:28:47 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,6 +1,9 @@
 package com.hemebiotech.analytics;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
@@ -15,4 +18,25 @@ public class AnalyticsCounter {
 	public List<String> getSymptoms() {
 		return reader.getSymptoms();
 	}
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> symptomCounts = new HashMap<String, Integer>();
+		for (String symptom : symptoms) {
+			symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
+		}
+		return symptomCounts;
+	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		Map<String, Integer> sortedListSymptoms = new TreeMap<String, Integer>();
+		for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+			sortedListSymptoms.put(entry.getKey(), entry.getValue());
+		}
+		return sortedListSymptoms;
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		writer.writeSymptoms(symptoms);
+	}
+
 }
