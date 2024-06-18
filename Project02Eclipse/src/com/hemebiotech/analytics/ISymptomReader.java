cb3a3bb1060687edb47515c3f6a8ca09210eb13b
laflosh <l_florent_l@hotmail.fr>
2024-06-18 14:39:06 +0200

    création branch dev + Aération code

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -10,11 +10,15 @@ import java.util.List;
  * The implementation does not need to order the list
  * 
  */
+
 public interface ISymptomReader {
+	
 	/**
 	 * If no data is available, return an empty List
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
+	
 	List<String> GetSymptoms ();
+	
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -10,6 +10,7 @@ import java.util.List;
  * Simple brute force implementation
  *
  */
+
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
@@ -18,27 +19,40 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
+	
 	public ReadSymptomDataFromFile (String filepath) {
+		
 		this.filepath = filepath;
+		
 	}
 	
 	@Override
 	public List<String> GetSymptoms() {
+		
 		ArrayList<String> result = new ArrayList<String>();
 		
 		if (filepath != null) {
+			
 			try {
+				
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
 				
 				while (line != null) {
+					
 					result.add(line);
 					line = reader.readLine();
+					
 				}
+				
 				reader.close();
+				
 			} catch (IOException e) {
+				
 				e.printStackTrace();
+				
 			}
+			
 		}
 		
 		return result;
