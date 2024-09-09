6ae3f455bc65584c7e8b41514bdae9e2d022c381
Laurent Cordier <laurent.cordier28@orange.fr>
2024-09-09 12:29:39 +0200

    remove unnecessary comments and obvious code fixes

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,36 +1,29 @@
-package com.hemebiotech.analytics;
+//package com.hemebiotech.analytics; // supprimé pour le moment car pb.
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
+	private static int pupilCount = 0;		
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
+		String line; 
+		
+		while ((line = reader.readLine()) != null) {
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
+			else if (line.contains("dialated pupils")) {
 				pupilCount++;
 			}
-
-			line = reader.readLine();	// get another symptom
 		}
 		
 		// next generate output
